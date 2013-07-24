930b61bce9d881524dca52f603eed2df2edbe072
Henrik Hodne <me@henrikhodne.com>
2013-07-24 15:06:54 -0700

    Include chruby conditionally

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -1,3 +1,5 @@
-source `brew --prefix`/opt/chruby/share/chruby/chruby.sh
-source `brew --prefix`/opt/chruby/share/chruby/auto.sh
-chruby $(cat ~/.ruby-version)
+if ! $(brew &>/dev/null); then
+  source `brew --prefix`/opt/chruby/share/chruby/chruby.sh
+  source `brew --prefix`/opt/chruby/share/chruby/auto.sh
+  chruby $(cat ~/.ruby-version)
+fi

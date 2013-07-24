c7fdb10fd1d14dd95caa5b2cfa5b0e831e6875aa
Henrik Hodne <me@henrikhodne.com>
2013-07-24 15:08:04 -0700

    Fix chruby condition

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -1,4 +1,4 @@
-if ! $(brew &>/dev/null); then
+if $(brew &>/dev/null); then
   source `brew --prefix`/opt/chruby/share/chruby/chruby.sh
   source `brew --prefix`/opt/chruby/share/chruby/auto.sh
   chruby $(cat ~/.ruby-version)
